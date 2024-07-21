c916f6600b2afe71e4a21946a8f72e12057a9f05
LJW20031212 <3282464094@qq.com>
2024-07-22 00:03:10 +0800

    Add files via upload

---
+++ test-7-21/test-7-21/test.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	/*int a = 0;
+	printf("%d\n", a);
+	printf("%zd\n", sizeof(long long));*/
+
+	/*float a = 3.145f;
+	printf("%f\n", a);*/
+
+	char ch = 'A';
+	printf("%c\n", ch);
+	return 0;
+}
